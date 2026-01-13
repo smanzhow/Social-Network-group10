ad5bc8a50c7a6781fa22d98d116d64d01fdad048
smanzhow <smanzhow@gmail.com>
2026-01-13 16:05:19 +0100

    homepage

---
+++ src/main/java/se/jensen/saman/socialnetworkmaven/controller/HomeController.java
@@ -0,0 +1,12 @@
+package se.jensen.saman.socialnetworkmaven.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Social Network API is running!";
+    }
+}
